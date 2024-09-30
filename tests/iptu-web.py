40be536f562e1f358db6fc3f379d5cf2a7b0fb8c
Diogo José Freitas de Oliveira <djfreitasoliveira@gmail.com>
2024-09-30 10:03:43 -0400

    teste-upload

---
+++ tests/iptu-web.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+from webscrapper import webscrapper
+
+driver = webscrapper()
+
+driver.get("https://iptu.campogrande.ms.gov.br/")
+
+sleep(4)
+if driver.input_text('[id="inputInscricao"]', 6830270050):
+    
+    print("chegou")
+else:
+    print("não chegou")
+
+
+driver.click_button('[type="submit"]')
+sleep(20)
