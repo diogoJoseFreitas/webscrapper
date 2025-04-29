18d7ab8a9c0e9228bea7a7c9ce9a6565f4fd9ee1
DiogoJoseFreitas <diogoJoseFreitas@proton.me>
2025-04-28 20:41:19 -0400

    better implementing of example

--- tests/iptu-web.py
+++ tests/iptu-web.py
@@ -1,18 +1,19 @@
 from time import sleep
 
-from webscrapper import webscrapper
+from webscrapper import Webscrapper
 
-driver = webscrapper()
+driver = Webscrapper()
 
 driver.get("https://iptu.campogrande.ms.gov.br/")
 
 # sleep(4)
 if driver.input_text('[id="inputInscricao"]', 6830270050):
-    
+
     print("chegou")
 else:
     print("não chegou")
 
-driver.click_button('[type="submit"]')
+driver.wait_for_object('/html/body/section[1]/div/div/div[2]/div[2]', by='xpath', loop_times=10)
 
-# sleep(20)
+driver.click_button('//*[@id="list-table-1"]/tbody/tr[2]/th/div/input', by='xpath')
+driver.click_button('[id="spanCopiarCodigoBarras"]')
